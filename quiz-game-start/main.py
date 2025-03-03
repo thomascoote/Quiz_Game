43971eb9e2e4a130faa3bf6325353372f4d7e1e1
tom <thomascoote89@gmail.com>
2025-03-03 21:52:36 +0000

    Day 17 quiz

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,13 +1,23 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
+import html
 import random
 
 question_bank = []
+answer_bank = []
 
 for i in question_data:
-    temp_q = i["text"]
-    temp_a = i["answer"]
+    temp_q = i["question"]
+    temp_q = html.unescape(temp_q)
+    temp_a = i["correct_answer"]
     question = Question(temp_q,temp_a)
     question_bank.append(question)
 
-print(question_bank[0].text)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"You've completed the quiz!\nYour final score was {quiz.score}/{quiz.question_number}")
